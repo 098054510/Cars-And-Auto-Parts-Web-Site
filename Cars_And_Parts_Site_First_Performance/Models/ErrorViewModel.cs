9b5ba5ce54a4bfc77f2b52e4784d7cf1af6da25f
Eduardo De Souza <eduardohsfreitas86@gmail.com>
2019-01-13 20:19:04 -0300

    Add project files.

---
+++ Cars_And_Parts_Site_First_Performance/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cars_And_Parts_Site_First_Performance.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
