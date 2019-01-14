04b68899220b6a75191349b6ea88516d4747956d
Eduardo De Souza <eduardohsfreitas86@gmail.com>
2019-01-13 23:18:42 -0300

    Creation of Database for Cars_And_AutoParts_Site sync with Visual Studio

---
+++ Cars_And_Parts_Site_First_Performance/Migrations/20190114021243_Cars_And_AutoParts_Shop_Web_Site.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cars_And_Parts_Site_First_Performance.Migrations
+{
+    public partial class Cars_And_AutoParts_Shop_Web_Site : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DepartmentModel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentModel", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentModel");
+        }
+    }
+}
